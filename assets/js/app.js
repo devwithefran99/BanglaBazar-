d36dd511bc1121e598b536ab42b6d236da9effa7
shawon <digital.erfan37@gmail.com>
2026-04-13 18:37:30 +0600

    done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,19 @@
+// preloader js
+window.addEventListener('load', function() {
+  const preloader = document.getElementById('preloader');
+  
+  // ১-২ সেকেন্ড পর অথবা পুরোপুরি লোড হলে লুকাবে
+  setTimeout(() => {
+    preloader.style.opacity = '0';
+    
+    // অ্যানিমেশন শেষ হলে পুরোপুরি remove
+    setTimeout(() => {
+      preloader.style.display = 'none';
+    }, 800);
+  }, 1500);   // এখানে সময় চেঞ্জ করতে পারো
+});
+// preloader js ends
+
 // navbar js
 document.querySelectorAll('.main-navbar .nav-item').forEach((el, i) => {
     el.style.opacity = '0';
@@ -10,3 +26,39 @@ document.querySelectorAll('.main-navbar .nav-item').forEach((el, i) => {
   });
 
 //   nav bar js ends
+
+// hero js down
+$(document).ready(function(){
+
+  let current = 0;
+  let slides = $(".slide");
+  let total = slides.length;
+
+  function showSlide(index){
+    slides.removeClass("active");
+    slides.eq(index).addClass("active");
+  }
+
+  $(".slider-next").click(function(){
+    current = (current + 1) % total;
+    showSlide(current);
+  });
+
+  $(".slider-prev").click(function(){
+    current = (current - 1 + total) % total;
+    showSlide(current);
+  });
+  
+  function showSlide(index){
+  $(".slide").removeClass("active");
+  $(".slide").eq(index).addClass("active");
+}
+
+  // auto slide
+  setInterval(function(){
+    current = (current + 1) % total;
+    showSlide(current);
+  }, 3000);
+
+});
+// hero part js ends
